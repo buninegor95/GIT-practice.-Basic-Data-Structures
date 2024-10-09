ab3e9a4c397406896ce3d340ead26f4d127d9107
buninegor95 <buninegor95@gmail.com>
2024-10-09 23:49:03 +0300

    Add files via upload

---
+++ GIT practice. Basic Data Structures.py
@@ -0,0 +1,38 @@
+# я не понял почему у меня не находится файл на платформе поэтому я оставлю его на githab
+
+# Практическая работа по уроку "Динамическая типизация"
+name = 'Михаил'
+age = 13
+New_Age = age + 1
+is_student = True
+print(f"name: {name}")
+print(f"age: {age}")
+print(f"New_Age: {New_Age}")
+print(f"is_student: {is_student}")
+
+# Практическое задание по теме "Переменные"
+Number_of_completed_homework = 12
+Number_of_hours_spent = 1.5 * 60
+Course_name = 'Python'
+Time_per_task = int(Number_of_hours_spent) / Number_of_completed_homework
+print(f"количество работ: {Number_of_completed_homework}")
+print(f"время потраченое на работы в минутах: {Number_of_hours_spent}")
+print(f"название курса: {Course_name}")
+print(f"на 1 задание затрачено минут: {Time_per_task}")
+
+# Практическое задание по теме "Строки и индексация строк"
+example = 'Indexing'
+print(f"example = 'indexing'")
+print(example[0])
+print(example[-1])
+print(example[5:])
+print(example[::-1])
+print(example[0:8:2])
+
+# Практическое задание по теме "Организация программ и методы строк"
+my_string = input("введите словосочетание :")
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(" ",""))
+print(my_string[0])
+print(my_string[-1])
